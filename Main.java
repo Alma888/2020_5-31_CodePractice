de3d1bea40b1391193dc7102f215e43a0e3781c7
Alma <2484094776qq.com>
2020-05-31 21:46:20 +0800

    线程应用

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        Thread t = new Thread() {
+            public void run() {
+                pong();
+            }
+        };
+        t.run();
+        System.out.print("ping");
+        
+    }
+    static void pong() {
+        System.out.print("pong");
+    }
+}
